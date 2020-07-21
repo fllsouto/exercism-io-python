13db18d987c11ad4128aeaae706b2151e0019d8a
Fellipe Souto <fllsouto@gmail.com>
2020-07-20 22:37:15 -0300

    hamming functional solution 2

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,10 +1,8 @@
-from itertools import starmap
-from functools import reduce
+from operator import ne
 
 def distance(strand_a, strand_b):
     if (len(strand_a) != len(strand_b)):
         raise ValueError("Strands should have equal size")
 
-    zipped_strands = zip(strand_a, strand_b)
-    bitmap_distance = starmap(lambda a, b: a != b, zipped_strands)
-    return reduce(lambda a, b: a + b, bitmap_distance, 0)
+    bitmap_distance = map(ne, strand_a, strand_b)
+    return sum(bitmap_distance)
